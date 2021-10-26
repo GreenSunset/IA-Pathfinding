62939e18b7c78491936ac149967c88b25513d816
DiegoDiazMoron <alu0101337975@ull.edu.es>
2021-10-27 00:10:30 +0100

    Ajuste de los canvas para el rescalado

--- Assets/Scripts/WorldInfo.cs
+++ Assets/Scripts/WorldInfo.cs
@@ -5,16 +5,16 @@ using UnityEngine;
 public class WorldInfo : MonoBehaviour
 {
     // Tamaño del mapa
-    public static Vector2Int Size = new Vector2Int(1,1);
+    public static Vector2Int Size = new Vector2Int(10,10);
     // Posicion del Comienzo y Final
     public static Vector2Int Beginning = new Vector2Int(-1, -1);
     public static Vector2Int End = new Vector2Int(-1, -1);
     // Posicion de los Obstaculos
     public static List<Vector2Int> Obstacles = new List<Vector2Int>();
     // Heuristica
-    public static HeuristicFunc Heuristic = new ManhatanHeuristic();
+    public static HeuristicFunc Heuristic = new EuclideanHeuristic();
     // Tipo de movimiento
-    public static bool DoDiagonals = false;
+    public static bool DoDiagonals = true;
     // Ajustes Manuales
     public static bool ManualObstacles = true;
     public static bool ManualObjectives = true;
